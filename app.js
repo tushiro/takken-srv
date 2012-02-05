d9fca0370a9391ccb2c859326d2180f869971232
tushiro <toshihiro.ushiro@gmail.com>
2012-02-05 20:27:41 +0900

    node sample and ajaxterm merged.

---
+++ app.js
@@ -0,0 +1,37 @@
+var fcgiApp = require("./fcgi");
+var http = require("http");
+var takkenUtil = require("./lib/takken_util");
+var categoryDB = require("./lib/category_db");
+var logger = takkenUtil.getLogger();
+var fs = require("fs");
+	
+var myServer = http.createServer(function(req, res) {
+	setTimeout(function() {
+		
+		var client = takkenUtil.getMySQLClient();
+        var categories = categoryDB.getAll(client);
+        client.end();
+        
+        var s = 'Hello World!';
+        logger.info(s);
+    
+        var str = "";
+        for(var i = 0; i < categories.length; i++) {
+          str += categories[i].category;
+        };
+    
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end(s);
+        console.log("Wrote response.");
+  
+	}, 1000);
+	//throw new Error("Bollocks.");
+});
+
+
+// Instead of this:
+//myServer.listen(12345);
+
+// You do this:
+fcgiApp.handle(myServer);
+
