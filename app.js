fa446d130636b4fbd898b876105fbbeb339e20aa
tushiro <toshihiro.ushiro@gmail.com>
2012-02-10 11:44:23 +0900

    event log test

--- app.js
+++ app.js
@@ -1,62 +1,38 @@
+
 var fcgiApp = require("./fcgi");
 var http = require("http");
-//var takkenUtil = require("./lib/takken_util");
-//var categoryDB = require("./lib/category_db");
-//var logger = takkenUtil.getLogger();
-var fs = require("fs");
-	
+
+var takkenUtil = require("./lib/takken_util");
+var categoryDB = require("./lib/category_db");
+
+var logger = takkenUtil.getLogger();
+
+
 var myServer = http.createServer(function(req, res) {
 	setTimeout(function() {
 		
-//		var client = takkenUtil.getMySQLClient();
-//        var categories = categoryDB.getAll(client);
-//        client.end();
-        
-        var s = 'Hello World test! ' + Date.now() + " ";
-//        logger.debug(s);
-//        logger.end();
-    
-
-//        try { 
-////            var stream = fs.createWriteStream('/home/takken-test/log/file.log', { flags: 'a' })
-//        	var fd = fs.openSync('/home/takken-test/log/file.log', "r");
-//          
-//        } catch (e) {
-//            s += 'openSync Error ' + e.stack;
-//            res.writeHead(200, {"Content-type": "text/html"});
-//            res.end(s);
-//            return;
-//        }
-        
         try { 
-        	s += fs.readFileSync('/home/takken-test/log/file.log');
+    		var client = takkenUtil.getMySQLClient();
+            var categories = categoryDB.getAll(client);
+            client.end();
         
-        } catch (e) {
-            s += 'readFileSync Error ' + e.stack;
+
+            var s = "";
+            for(var i = 0; i < categories.length; i++) {
+              s += categories[i].category;
+            };
+            
             res.writeHead(200, {"Content-type": "text/html"});
             res.end(s);
+            console.log("Wrote response.");
+
+        } catch (e) {
+            logger.error(e.stack);
+            res.writeHead(200, {"Content-type": "text/html"});
+            res.end(e.stack);
             return;
         }
-
-//        try { 
-////        	fs.end();
-//        	fs.closeSync(fd);
-//            
-//        } catch (e) {
-//            s += 'closeSync Error ' + e.stack;
-//            res.writeHead(200, {"Content-type": "text/html"});
-//            res.end(s);
-//            return;
-//        }
-
-//        var str = "";
-//        for(var i = 0; i < categories.length; i++) {
-//          str += categories[i].category;
-//        };
-    
-        res.writeHead(200, {"Content-type": "text/html"});
-        res.end(s);
-        console.log("Wrote response.");
+        
   
 	}, 1000);
 	//throw new Error("Bollocks.");
