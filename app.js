47e230be42122c8136c8ca5b9694a41d998b3dfb
tushiro <toshihiro.ushiro@gmail.com>
2012-02-19 15:05:20 +0900

    updated

--- app.js
+++ app.js
@@ -1,72 +1,20 @@
 
 var fcgiApp = require("./fcgi");
 var http = require("http");
-var async = require('async');
 
 var takkenUtil = require("./lib/takken_util");
 var logger = takkenUtil.getLogger();
 
-var categoryDB = require("./lib/category_db");
-var subjectDB = require("./lib/subject_db");
-var questionDB = require("./lib/question_db");
-var sentenceDB = require("./lib/sentence_db");
+var responseJson = require("./lib/response_json");
 
 
 var myServer = http.createServer(function(req, res) {
+
 	setTimeout(function() {
 
-		try {
+        try {
             
-            var tasks = [
-                function (callback) {
-                    
-                    try {
-                        var client = takkenUtil.getMySQLClient();
-                        
-                    } catch (e) {
-                        callback(e);
-                        return;                     
-                    }
-                 
-                    var cache = [];
-                    
-                    callback(null, client, cache);
-                },
-                function (client, cache, callback) {
-                    subjectDB.pushTo(client, cache, callback);
-                },
-                function (client, cache, callback) {
-                    categoryDB.pushTo(client, cache, callback);
-                },
-                function (client, cache, callback) {
-                    questionDB.pushTo(client, cache, callback);
-                },
-                function (client, cache, callback) {
-                    sentenceDB.pushTo(client, cache, callback);
-                },
-                function (client, cache, callback) {
-
-                    client.end();
-                    
-                    var message = JSON.stringify(cache);
-                    
-                    res.writeHead(200, {"Content-type": "application/json"});
-                    res.end(message);
-                    
-                    logger.info("send json data");
-                }
-            ];            
-            
-            var errorHandle = function (err) {
-    
-                if (client != null) {
-                    client.end();
-                }
-
-                logger.error(err);
-            };
-            
-            async.waterfall(tasks, errorHandle);
+            responseJson.execute(req, res);
             
         } catch (e) {
             logger.error(e.stack);
